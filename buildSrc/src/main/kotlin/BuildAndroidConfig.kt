2545606b9086cb77024e0b87270a14b2c907c497
ulite-Amr <aamar302@yahoo.com>
2023-02-03 16:48:20 +0200

    trying 2

--- buildSrc/src/main/kotlin/BuildAndroidConfig.kt
+++ buildSrc/src/main/kotlin/BuildAndroidConfig.kt
@@ -4,7 +4,7 @@ object BuildAndroidConfig {
     const val COMPILE_SDK_VERSION = 33
     const val MIN_SDK_VERSION = 26
     const val TARGET_SDK_VERSION = 33
-    const val BUILD_TOOLS_VERSION = "33.0.3"
+    const val BUILD_TOOLS_VERSION = "33.0.0"
 
     const val VERSION_CODE = 2
     
