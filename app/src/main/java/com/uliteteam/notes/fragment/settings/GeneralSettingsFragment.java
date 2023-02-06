4955ef55133fe51e589abb99c93d7a00156b91fb
ulite-Amr <aamar302@yahoo.com>
2023-02-06 17:47:25 +0200

    add [General] settings

---
+++ app/src/main/java/com/uliteteam/notes/fragment/settings/GeneralSettingsFragment.java
@@ -0,0 +1,24 @@
+package com.uliteteam.notes.fragment.settings;
+
+import android.os.Bundle;
+import androidx.preference.PreferenceFragmentCompat;
+
+import com.google.android.material.transition.MaterialSharedAxis;
+import com.uliteteam.notes.R;
+import com.uliteteam.notes.fragment.settings.BasePreferenceFragment;
+
+public class GeneralSettingsFragment extends BasePreferenceFragment {
+
+
+  public GeneralSettingsFragment() {
+    super(R.string.pref_general);
+  }
+
+  @Override
+  public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+    setPreferencesFromResource(R.xml.pref_general, rootKey);
+        
+    setEnterTransition(new MaterialSharedAxis(MaterialSharedAxis.X, false));
+    setExitTransition(new MaterialSharedAxis(MaterialSharedAxis.X, true));
+  }
+}
