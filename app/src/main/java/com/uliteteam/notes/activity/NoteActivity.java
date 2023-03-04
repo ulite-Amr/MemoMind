0f739f9ea22c507aa55e5cd23af35b34847e92a3
ulite-Amr <aamar302@yahoo.com>
2023-03-04 17:20:21 +0200

    Lol tany

--- app/src/main/java/com/uliteteam/notes/activity/NoteActivity.java
+++ app/src/main/java/com/uliteteam/notes/activity/NoteActivity.java
@@ -1,5 +1,9 @@
 package com.uliteteam.notes.activity;
 
+ /**
+        Created By Amr Ayman
+        
+ **/
 import android.content.res.Configuration;
 import android.icu.util.Calendar;
 import android.os.Bundle;
